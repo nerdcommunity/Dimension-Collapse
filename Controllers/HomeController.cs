18f21fdf681e392ca3605c4b97b5bc34f6f091f6
AlTrAugsburg <albert@ladon.xyz>
2019-08-04 19:48:02 +0200

    Game Seite hinzugefügt. In den weißen Bereich kommt das Bild der Einheit rein, in den grünen die Infos. Wird noch verfeinert und bearbeitet. Feldgröße: 10 (Höhe) * 20 (Weite)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,25 +16,6 @@ namespace Dimension_Collapse.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
